7d2af72efe72b3a381ec0e38743c145468859509
HilstonWill <hwchihuantito@gmail.com>
2025-09-26 12:41:13 -0500

    Update config.php

--- backEndYUPAY/config.php
+++ backEndYUPAY/config.php
@@ -1,7 +1,8 @@
 <?php
-define('DB_SERVER', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'bd_yupay');
-define('DB_PORT', 3306);
+define('DB_SERVER', getenv('DB_HOST') ?: 'localhost');
+define('DB_PORT', intval(getenv('DB_PORT') ?: 3306));
+define('DB_USER', getenv('DB_USER') ?: 'root');
+define('DB_PASS', getenv('DB_PASS') ?: '');
+define('DB_NAME', getenv('DB_NAME') ?: 'bd_yupay');
+
 ?>
